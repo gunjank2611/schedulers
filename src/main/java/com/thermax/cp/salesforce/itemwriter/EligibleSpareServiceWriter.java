824d4be07b2b83dd3ae2476f98c28dd87f44e5a7
Anand S <anand.s@nagarro.com>
2021-10-22 17:05:53 +0530

    fix the build issue

--- src/main/java/com/thermax/cp/salesforce/itemwriter/EligibleSpareServiceWriter.java
+++ src/main/java/com/thermax/cp/salesforce/itemwriter/EligibleSpareServiceWriter.java
@@ -24,14 +24,16 @@ public class EligibleSpareServiceWriter implements ItemWriter<SFDCEligibleSpares
     @Override
     public void write(List<? extends SFDCEligibleSparesServicesDTO> eligibleSparesServicesDTOS) throws Exception {
         log.info("Saving data for eligible spares and services of size: {} ", eligibleSparesServicesDTOS.size());
-         String[] headers = new String[]{"id", "asset__c", "currencyIsoCode", "life_Cycle_Date__c", "name", "part_Number__c","tH_Thermax_Spare__c",
+        String[] headers = new String[]{"id", "asset__c", "currencyIsoCode", "life_Cycle_Date__c", "name", "part_Number__c", "tH_Thermax_Spare__c",
                 "type__c", "when_to_Engage_Customer__c"};
-        final String fileName="eligibleSparesServices.csv";
-        final String apiName="EligibleSparesServices";
-        CompletableFuture<String> url = csvWrite.writeToCSV(eligibleSparesServicesDTOS,headers,fileName,apiName);
+        final String fileName = "eligibleSparesServices.csv";
+        final String apiName = "EligibleSparesServices";
+        CompletableFuture<String> url = csvWrite.writeToCSV(eligibleSparesServicesDTOS, headers, fileName, apiName);
         log.info("Written Eligible Spares Services to the file : {}", url.get());
-        FileURLDTO fileURLDTO=new FileURLDTO();
+        FileURLDTO fileURLDTO = new FileURLDTO();
         fileURLDTO.setFileUrl(url.get());
         enquiryConnector.sendEligibleSparesServiceUrl(fileURLDTO);
         log.info("Pushed Eligible Spares Service data to DB !");
+    }
+
 }
