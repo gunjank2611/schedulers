089e28e5db8a0a8834c638c22cc11f66b7b8e49e
ashish.dua <ashish.dua@nagarro.com>
2021-11-08 18:28:10 +0000

    Updated app-logging.sh

--- Devops/app-logging.sh
+++ Devops/app-logging.sh
@@ -4,9 +4,8 @@ az login --use-device-code
 az account set --subscription 90a5cf8f-aa05-450d-9850-5d64c0f061d9
 az aks get-credentials --overwrite --resource-group rgaz-cin-tcp-qa --name aks-cin-thermax-qa
 kubectl config set-context --current --namespace=thermax
-OLDIFS=$IFS
-IFS=$'\n'
-for app_name in $(cat /home/appnames.txt)
+sed 's/\r$//' /home/appnames.txt>/home/app.txt
+for app_name in $(cat /home/app.txt)
 do
   success_status=$(kubectl get pod --field-selector status.phase=Running --no-headers|grep "$appname"|head -1)
   echo $success_status
@@ -14,11 +13,11 @@ do
   echo $app_name
   if [[ $success_status == *"No resources"* ]]
   then 
-    mkdir -p /opt/scripts/$(echo $app_name|tr -dc '[:alnum:]\n\r')/failure/$CURRENT_DATE
+    mkdir -p /opt/scripts/failure/$CURRENT_DATE
     kubectl logs --selector=app=$app_name -c $app_name -n thermax |grep -i -e error -e warning>/opt/scripts/$app_name/failure/$CURRENT_DATE/error.log
     find /opt/scripts/$app_name/failure -type d -mtime +1 | xargs rm -rf
   else
-    mkdir -p /opt/scripts/$(echo $app_name|tr -dc '[:alnum:]\n\r')/success/$CURRENT_DATE
+    mkdir -p /opt/scripts/success/$CURRENT_DATE
     kubectl logs --selector=app=$app_name -c $app_name -n thermax>/opt/scripts/$app_name/success/$CURRENT_DATE/success.log
     find /opt/scripts/$app_name/success -type d -mtime +1 | xargs rm -rf
   fi
