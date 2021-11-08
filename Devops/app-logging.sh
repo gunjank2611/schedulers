7c90ab1f22fa7ad0c1c95e33da1194af67162254
ashish.dua <ashish.dua@nagarro.com>
2021-11-08 17:55:43 +0000

    Updated app-logging.sh

--- Devops/app-logging.sh
+++ Devops/app-logging.sh
@@ -4,6 +4,8 @@ az login --use-device-code
 az account set --subscription 90a5cf8f-aa05-450d-9850-5d64c0f061d9
 az aks get-credentials --overwrite --resource-group rgaz-cin-tcp-qa --name aks-cin-thermax-qa
 kubectl config set-context --current --namespace=thermax
+OLDIFS=$IFS
+IFS=$'\n'
 for app_name in $(cat /home/appnames.txt)
 do
   success_status=$(kubectl get pod --field-selector status.phase=Running --no-headers|grep "$appname"|head -1)
@@ -21,3 +23,4 @@ do
     find /opt/scripts/$app_name/success -type d -mtime +1 | xargs rm -rf
   fi
 done
+IFS=$OLDIFS
