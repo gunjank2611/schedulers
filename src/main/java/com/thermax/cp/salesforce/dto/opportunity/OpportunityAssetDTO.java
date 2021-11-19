cb4ed7cad0da2fa939603a30bc45da3de11a3ad1
gunjankhatter <gunjan.khatter@nagarro.com>
2021-11-19 12:11:07 +0530

    opportunity asset

--- src/main/java/com/thermax/cp/salesforce/dto/opportunity/OpportunityAssetDTO.java
+++ src/main/java/com/thermax/cp/salesforce/dto/opportunity/OpportunityAssetDTO.java
@@ -1,25 +1,16 @@
 package com.thermax.cp.salesforce.dto.opportunity;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.thermax.cp.salesforce.dto.account.SFDCAccountInfoDTO;
-import com.thermax.cp.salesforce.dto.commons.AttributesDTO;
 import lombok.Data;
+import lombok.Getter;
 
 import java.util.List;
 
 @Data
+@Getter
+
 public class OpportunityAssetDTO {
     private String totalSize;
     private String done;
     private List<SFDCOpportunityAssetDTO> records;
 }
-@Data
-class SFDCOpportunityAssetDTO
-{
-    private AttributesDTO attributes;
-    @JsonProperty("Id")
-    private String id;
-    @JsonProperty("Asset__c")
-    private String assetId;
 
-}
