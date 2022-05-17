9a5390f50406d275f62f5e64c315e0947482146c
gunjankhatter <gunjan.khatter@nagarro.com>
2022-05-17 18:49:45 +0530

    erp

--- src/main/java/com/thermax/cp/salesforce/itemreader/ErpOrderStatusReader.java
+++ src/main/java/com/thermax/cp/salesforce/itemreader/ErpOrderStatusReader.java
@@ -3,11 +3,13 @@ package com.thermax.cp.salesforce.itemreader;
 import com.thermax.cp.salesforce.dto.orders.ErpOrderStatusDTO;
 import com.thermax.cp.salesforce.dto.orders.PageNumberDTO;
 import com.thermax.cp.salesforce.dto.orders.SFDCOrderHeadersDTO;
+import com.thermax.cp.salesforce.dto.services.SFDCServiceLogDTO;
 import com.thermax.cp.salesforce.feign.request.ErpOrderStatusRequest;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.http.ResponseEntity;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Log4j2
@@ -17,63 +19,65 @@ public class ErpOrderStatusReader implements ItemReader<SFDCOrderHeadersDTO> {
     private final String frequency;
     private boolean isOrderStatusInitialized;
     private List<SFDCOrderHeadersDTO> sfdcOrderHeadersDTOs;
-    private int index = 0;
+    private int nextErpOrderIndex = 0;
     private long totalRecords = 0;
 
     public ErpOrderStatusReader(ErpOrderStatusRequest erpOrderStatusRequest, String frequency) {
         this.erpOrderStatusRequest = erpOrderStatusRequest;
         this.frequency = frequency;
+        this.nextErpOrderIndex=0;
     }
-
     @Override
     public SFDCOrderHeadersDTO read() throws Exception {
 
-        log.info("Inside ErpOrderStatusReader -> read()..");
+        if(isOrderStatusInitialized())
+        {
+            sfdcOrderHeadersDTOs =getERPOrderRecords(frequency);
+        }
+        SFDCOrderHeadersDTO nextErpOrder;
+        if (nextErpOrderIndex < sfdcOrderHeadersDTOs.size()) {
+            nextErpOrder = sfdcOrderHeadersDTOs.get(nextErpOrderIndex);
+            nextErpOrderIndex++;
+        }
+        else {
+            nextErpOrderIndex = 0;
+            nextErpOrder = null;
+        }
 
-        if (!isOrderStatusInitialized) {
+        return nextErpOrder;
+    }
 
-            log.info("ErpOrderStatus is not initialized !!");
+    private boolean isOrderStatusInitialized()
+    {
+        return this.sfdcOrderHeadersDTOs ==null;
+    }
 
-            long totalCount;
 
-            // Initial PageRequest..
-            int pageNumber = 1;
-            PageNumberDTO pageNumberDTO = new PageNumberDTO(pageNumber);
-            ResponseEntity<ErpOrderStatusDTO> erpOrderStatusHeaderResponseEntity =
-                    erpOrderStatusRequest.fetchOrderStatus(pageNumberDTO);
+    private List<SFDCOrderHeadersDTO> getERPOrderRecords(String frequency) {
+        long totalCount;
+        int pageNumber = 1;
+        PageNumberDTO pageNumberDTO = new PageNumberDTO(pageNumber);
+        ResponseEntity<ErpOrderStatusDTO> erpOrderStatusHeaderResponseEntity =
+                erpOrderStatusRequest.fetchOrderStatus(pageNumberDTO);
+        List<SFDCOrderHeadersDTO> erpOrdersList = new ArrayList<SFDCOrderHeadersDTO>();
+        if (erpOrderStatusHeaderResponseEntity.hasBody()) {
+            ErpOrderStatusDTO erpOrderStatusDTO = erpOrderStatusHeaderResponseEntity.getBody();
+            erpOrdersList.addAll(erpOrderStatusDTO.getOrdersList());
 
-            if (erpOrderStatusHeaderResponseEntity.hasBody()) {
-                ErpOrderStatusDTO erpOrderStatusDTO = erpOrderStatusHeaderResponseEntity.getBody();
-                totalCount = erpOrderStatusDTO.getTotalCount();
-                totalCount = decrementTotalCount(totalCount, erpOrderStatusDTO.getOrdersList().size());
+            totalCount = decrementTotalCount(erpOrderStatusDTO.getTotalCount(), erpOrderStatusDTO.getOrdersList().size());
 
-                // Loop until there are more order statuses available..
-                while (totalCount > 0) {
-                    ResponseEntity<ErpOrderStatusDTO> nextErpOrderStatusResponseEntity =
-                            erpOrderStatusRequest.fetchOrderStatus(new PageNumberDTO(++pageNumber));
-                    if (nextErpOrderStatusResponseEntity.hasBody()) {
-                        ErpOrderStatusDTO nextErpOrderStatusDTO = nextErpOrderStatusResponseEntity.getBody();
-                        erpOrderStatusDTO.getOrdersList().addAll(nextErpOrderStatusDTO.getOrdersList());
-                        totalCount = decrementTotalCount(totalCount, nextErpOrderStatusDTO.getOrdersList().size());
-                    }
+            // Loop until there are more order statuses available..
+            while (totalCount > 0) {
+                ResponseEntity<ErpOrderStatusDTO> nextErpOrderStatusResponseEntity =
+                        erpOrderStatusRequest.fetchOrderStatus(new PageNumberDTO(++pageNumber));
+                if (nextErpOrderStatusResponseEntity.hasBody()) {
+                    ErpOrderStatusDTO nextErpOrderStatusDTO = nextErpOrderStatusResponseEntity.getBody();
+                    erpOrdersList.addAll(nextErpOrderStatusDTO.getOrdersList());
+                    totalCount = decrementTotalCount(totalCount, nextErpOrderStatusDTO.getOrdersList().size());
                 }
-                sfdcOrderHeadersDTOs = erpOrderStatusDTO.getOrdersList();
-                totalRecords = erpOrderStatusDTO.getTotalCount();
             }
-
-            log.info("Total record(s) fetched from ERP Endpoint -> " + totalRecords);
-            isOrderStatusInitialized = true;
         }
-
-        SFDCOrderHeadersDTO sfdcOrderHeadersDTO;
-        if (index < totalRecords) {
-            sfdcOrderHeadersDTO = sfdcOrderHeadersDTOs.get(index);
-            index++;
-        } else {
-            sfdcOrderHeadersDTO = null;
-        }
-
-        return sfdcOrderHeadersDTO;
+        return erpOrdersList;
     }
 
     private long decrementTotalCount(long totalCount, long nextSize) {
